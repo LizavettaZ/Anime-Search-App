130031ed80b2633ba6e65fe53f44d7e591a82492
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-04 14:53:38 +0300

    create Loader

---
+++ src/components/UI/Loader.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from '../../style/components/Loader.module.scss'
+
+const Loader = () => {
+  return (
+    <div className={classes.Loader}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loader
