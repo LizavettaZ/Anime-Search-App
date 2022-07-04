50ffd43f54c2d37b08d6e60d8602ac7215592372
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-04 14:54:12 +0300

    add context

--- src/App.js
+++ src/App.js
@@ -6,22 +6,28 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Anime from './pages/Anime'
 import Search from './pages/Search'
-import Contact from "./pages/Contact"
+import Contact from './pages/Contact'
+import AlertState from './context/alert/AlertState'
+import RequestState from './context/request/RequestState'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className= {classes.App}>
-        <NavMenu/>
-        <Routes>
-          <Route path='/' exact="true" element={<Home/>}/>
-          <Route path='/search' element={<Search/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/profile:id' element={<Anime/>}/>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <RequestState>
+      <AlertState>
+        <BrowserRouter>
+          <div className= {classes.App}>
+            <NavMenu/>
+            <Routes>
+              <Route path='/' exact="true" element={<Home/>}/>
+              <Route path='/search' element={<Search/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/contact' element={<Contact/>}/>
+              <Route path='/profile/slug' element={<Anime/>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </RequestState>
   )
 }
 
