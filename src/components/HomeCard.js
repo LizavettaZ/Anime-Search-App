83ae75944651aae2a777f3a59824f46ab751cb99
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-05 17:35:16 +0300

    completed HomePage & HomeCard

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import classes from '../style/HomeCard.module.scss'
+import classes from '../style/components/HomeCard.module.scss'
+import { Link } from 'react-router-dom'
 
-const HomeCard = () => {
+
+const HomeCard = ({ anime }) => {
+  const { posterImage } = anime.attributes
+
   return (
     <div className={classes.HomeCard}>
-      fdfdfdf
+      <div className={classes.card__img}>
+        <Link to= {'anime/:' + anime.id}>
+          <img src={posterImage.medium} alt = "Anime picture"/>
+        </Link>
+      </div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, {Fragment, useContext, useEffect} from 'react'
+import React, { Fragment, useContext, useEffect } from 'react'
 import Header from '../components/UI/Header'
 import classes from '../style/pages/Home.module.scss'
 import Button from '../components/UI/Button'
 import HomeCard from '../components/HomeCard'
-import {RequestContext} from '../context/request/requestContect'
+import { RequestContext } from '../context/request/requestContect'
 import Loader from '../components/UI/Loader'
 
+
 const Home = () => {
-  const {loading, popularAnime} = useContext(RequestContext)
+  const { loading, popularAnime } = useContext(RequestContext)
   const request = useContext(RequestContext)
 
   useEffect(() => {
-    request.getPopularAnime(10)
-    console.log('getPopularAnime render again')
+    request.getPopularAnime(4)
   }, [])
 
   return (
