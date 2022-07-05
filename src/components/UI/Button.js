37f12e46bb9eaa44cd20edb8f38b45f9ba478298
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-05 17:36:40 +0300

    update Button

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import classes from '../../style/Button.module.scss'
-import {NavLink} from "react-router-dom"
+import classes from '../../style/components/Button.module.scss'
+import { NavLink } from 'react-router-dom'
 
 const Button = () => {
   return (
