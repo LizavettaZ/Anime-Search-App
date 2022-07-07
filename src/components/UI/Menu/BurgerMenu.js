3b57624ac908f7877d5f9e4aac9515239b594c85
LizavettaZ <ehkizaveta13@gmail.com>
2022-07-07 12:34:03 +0300

    completed Menu

---
+++ src/components/UI/Menu/BurgerMenu.js
@@ -0,0 +1,15 @@
+import React, {useCallback} from 'react'
+import classes from '../../../style/components/Menu.module.scss'
+
+const BurgerMenu = ({ menuActive, setMenuActive }) => {
+
+  const onClick = useCallback(() => setMenuActive(!menuActive), [menuActive])
+
+  return (
+    <div className={classes.BurgerMenu} onClick={onClick}>
+      <span/>
+    </div>
+  )
+}
+
+export default BurgerMenu
--- src/components/UI/Menu/NavMenu.js
+++ src/components/UI/Menu/NavMenu.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import classes from '../../../style/Menu.module.scss'
-import {NavLink} from "react-router-dom";
+import React, {Fragment, useCallback, useState} from 'react'
+import classes from '../../../style/components/Menu.module.scss'
+import { NavLink } from 'react-router-dom'
+import BurgerMenu from './BurgerMenu'
+
 
 const NavMenu = () => {
+  const [menuActive, setMenuActive] = useState(false)
+
+  const cls = [
+    classes.NavMenu_list,
+    menuActive ? classes.active : null
+  ]
+
+  const onClick = useCallback(() => setMenuActive(false), [])
+
   return (
-    <nav className={classes.NavMenu}>
-      <ul className="NavMenu_list">
-        <li className="list__item">
-          <NavLink to="/" className="list__link"> Home </NavLink>
-        </li>
-        <p></p>
-        <li className="list__item">
-          <NavLink to= "/about" className="list__link"> About </NavLink>
-        </li>
-        <p></p>
-        <li className="list__item">
-          <NavLink to = "/contact" className="list__link"> Contact us </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <Fragment>
+      <nav className={classes.NavMenu}>
+        <BurgerMenu menuActive={menuActive} setMenuActive={setMenuActive}/>
+        <ul className={cls.join(' ')} onClick={onClick}>
+          <li className={classes.list__item}>
+            <NavLink to="/"> Home </NavLink>
+          </li>
+          <li className={classes.list__item}>
+            <NavLink to= "/about"> About </NavLink>
+          </li>
+          <li className={classes.list__item}>
+            <NavLink to = "/contact"> Contact us </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </Fragment>
   )
 }
 
